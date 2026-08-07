771e43961db650ed7d6bccde2c856244e86233b3
Divyansh Vishwakarma <divyanshvishwa1721@gmail.com>
2026-08-07 05:58:09 +0530

    Update accessories layout to 2 columns grid on mobile

--- src/app/accessories/page.tsx
+++ src/app/accessories/page.tsx
@@ -17,7 +17,7 @@ export default function AccessoriesPage() {
         </p>
       </div>
       
-      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="grid grid-cols-2 gap-x-3 gap-y-6 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {jewelryCollection.map((item) => (
           <AccessoryCard 
             key={item.id}
